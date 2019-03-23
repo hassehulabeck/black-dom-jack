a471f0ecfc19e2311c10cfae6e72aea31437306e
hassehulabeck <hansbudandersson@gmail.com>
2019-03-23 21:21:16 +0100

    Dealer deals to 17

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var deck = [];
 var suits = ["&spades;", "&hearts;", "&clubs;", "&diams;"];
 var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-var hand = [];
+var player = [];
+var dealer = [];
 let summa = 0;
 
 // När vi laddat in allt.
@@ -10,15 +11,40 @@ document.addEventListener('DOMContentLoaded', function() {
     createDeck();
     shuffle(deck);
     // console.log(deck);
-    deal(2);
-    // console.log(hand);
-    renderHand();
+    deal(player, 2);
+    deal(dealer, 1);
 
-    var button = document.getElementsByTagName('button');
-    button[0].addEventListener('click', () => {
-        deal(1);
-        renderHand();
+    // console.log(hand);
+    renderHand(player, "main");
+    renderHand(dealer, "dealerArea");
+
+    var button = document.getElementById('hit');
+    button.addEventListener('click', () => {
+        deal(player, 1);
+        renderHand(player, "main");
     })
+
+    // Eller via mellanslag.
+    document.addEventListener("keyup", event => {
+        if (event.key === " ") {
+            deal(player, 1);
+            renderHand(player, "main");
+        }
+    });
+
+    /* Stop button */
+    var stopBtn = document.getElementById('stop');
+    stopBtn.addEventListener('click', () => {
+        stop();
+    })
+
+    // Eller via ENTER.
+    document.addEventListener("keyup", event => {
+        if (event.key === "Enter") {
+            stop();
+        }
+    });
+
 
 });
 
@@ -36,12 +62,23 @@ function Card(suit, rank) {
     this.rank = rank;
 }
 
+function stop() {
+
+    let x = setInterval(function() {
+        deal(dealer, 1);
+        renderHand(dealer, "dealerArea");
+        if (summera(dealer) >= 17) {
+            clearInterval(x);
+        }
+    }, 1000);
+
+}
 
 
-function renderHand() {
-    let mainArea = document.getElementsByTagName('main');
+function renderHand(hand, area) {
+    let place = document.getElementById(area);
     // Börja med att rensa innehållet.
-    mainArea[0].innerHTML = "";
+    place.innerHTML = "";
 
     hand.forEach((kort) => {
             let card = document.createElement('div');
@@ -52,44 +89,71 @@ function renderHand() {
           </section>`;
             card.classList.add('card');
             // Svart eller rött kort?
-            if (kort.suit == '&spades;' || kort.suit == '&clubs') {
+            if (kort.suit == '&spades;' || kort.suit == '&clubs;') {
                 card.classList.add('black');
             } else {
                 card.classList.add('red');
             }
-            mainArea[0].appendChild(card);
+            place.appendChild(card);
         })
         // Skriv ut kortens värde.
-    let summaArea = document.getElementsByTagName('header');
-    summaArea[0].innerHTML = summera();
+    let summaArea;
+    if (area == 'main') {
+        summaArea = document.getElementsByTagName('header');
+        summaArea[0].innerHTML = summera(player);
+    } else {
+        summaArea = document.getElementsByTagName('footer');
+        summaArea[0].innerHTML = summera(dealer);
+    }
 }
 
-function deal(n) {
+function deal(hand, n) {
     for (let i = 0; i < n; i++) {
         hand.push(deck.shift());
     }
 }
 
-function summera() {
+function summera(hand) {
     summa = 0;
+    let numberOfAces = 0;
     let altString = "";
-    let gotAce = false;
 
     hand.forEach((kort) => {
-        // Räkna ut kortets valör (rank) och lägg till summa.
-        if (kort.rank == 'A') {
-            summa += 11;
-            gotAce = true;
-        } else if (ranks.indexOf(kort.rank) > '8') {
-            summa += 10;
-        } else {
-            summa += ranks.indexOf(kort.rank) + 2;
+            // Räkna ut kortets valör (rank) och lägg till summa.
+            if (kort.rank == 'A') {
+                summa += 11;
+                numberOfAces++;
+            } else if (ranks.indexOf(kort.rank) > '8') {
+                summa += 10;
+            } else {
+                summa += ranks.indexOf(kort.rank) + 2;
+            }
+        })
+        /* Kolla om spelaren är tjock (över 21)
+            Isåfall, kolla om det finns ett ess.
+            Om ess finns, minska summan med 10.
+        */
+
+    if (numberOfAces == 0) {
+        if (summa > 21) {
+            altString = `och är tjock.`;
         }
-    })
-    if ((gotAce) && (summa == 21)) {
-        altString = ` (eller ${summa-10})`;
+        if (summa == 21) {
+            altString = ``;
+        }
+    } else {
+        for (let i = 0; i < numberOfAces; i++) {
+            if (summa > 21) {
+                summa = summa - 10;
+            } else if (summa == 21) {
+                altString = `(Black jack)`;
+            } else {
+                altString = `(eller ${summa-10})`;
+            }
+        }
     }
-    return `Spelaren har ${summa} ${altString}`;
+
+    return summa;
 }
 
 
